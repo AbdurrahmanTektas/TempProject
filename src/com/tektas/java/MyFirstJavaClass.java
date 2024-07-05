5c510584017759b1187f47200f31e501ae679b5f
abdurrahman <abdr.tkts@gmail.com>
2024-07-05 18:45:27 +0200

    my third commmit

--- src/com/tektas/java/MyFirstJavaClass.java
+++ src/com/tektas/java/MyFirstJavaClass.java
@@ -3,7 +3,5 @@ package com.tektas.java;
 public class MyFirstJavaClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World 2");
     }
 }
